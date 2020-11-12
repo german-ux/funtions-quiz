3c10889f0f8750c843461ded136c34ad229a34da
german <66699192+german-ux@users.noreply.github.com>
2020-11-12 14:56:13 -0800

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,23 @@
+def area(width, height):
+    result = width * height
+    return result
+
+result = area(5, 6)
+print(result)
+
+area(6,7)
+area(9,5)
+
+def subtract(num1, num2):
+  result = num1 - num2
+  return result
+
+print(subtract(10,5))
+print(subtract(15,7))
+
+def add(num1, num2):
+  sum = num1 + num2
+  return sum
+
+print(add(5,14))
+print(add(2,5))
